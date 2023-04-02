c6a35d276064beb0a2202f6d947c54542b155092
tanhoangthinhmedia <127386274+tanhoangthinhmedia@users.noreply.github.com>
2023-04-02 16:44:37 +0700

    Update nopass.sh

--- nopass.sh
+++ nopass.sh
@@ -86,7 +86,7 @@ upload_proxy() {
 }
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "proxy/oBVvYc6Bl5/$IP4/$port/$(gen64 $IP6)"
+        echo "proxy/XG3rbLsNaV/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
